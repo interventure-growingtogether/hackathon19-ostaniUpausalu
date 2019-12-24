a333d731dabc81e155fb23c0f68d4f1f8023a62c
aleksandararnautovic <aleksandar.arnautovic@homegate.ch>
2019-12-24 11:02:10 +0100

    fix

--- data/db-handler.php
+++ data/db-handler.php
@@ -1,9 +1,6 @@
 <?php header('Content-Type: application/json');
 
-/* IMPORTANT:
- * change this to the main url of where you host the application, otherwise, every entry will be marked as a cheater
-*/
-$hostdomain = 'pacman.platzh1rsch.ch';
+$hostdomain = 'igrice.org';
 
 if (isset($_POST['action'])) {
 	switch ($_POST['action']) {
